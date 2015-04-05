a743495273b568c55aa1a50a4eb3d66fd7db3300
Melvin <melvin0008@gmail.com>
2015-04-05 19:12:57 +0530

    add the main module

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 from webob import Request
 req = Request.blank('http://localhost/test')
 resp = req.get_response(application)
-print resp
+
+
+if __name__ == '__main__':
+    from paste import httpserver
+    httpserver.serve(app, host='127.0.0.1', port=8080)
